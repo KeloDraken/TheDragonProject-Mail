ed1f5ae21b32a2d4221be515fa06c99ac10e7e36
Joy Mama-Kgosiemang <jmama021@student.wethinkcode.co.za>
2022-08-10 14:10:24 +0200

    Remove setItem from GoogleOAuth class

--- vetkoek/src/App.tsx
+++ vetkoek/src/App.tsx
@@ -1,12 +1,51 @@
+import axios from "axios";
+import { useCookies } from "react-cookie";
 import { GoogleLogin } from "react-google-login";
-import { SafeAreaView, StatusBar, Text, useColorScheme } from "react-native";
-import { handleLogin } from "./utils/sdk";
+import {
+  Platform,
+  SafeAreaView,
+  StatusBar,
+  Text,
+  useColorScheme,
+} from "react-native";
+import { GoogleOAuth } from "./utils/sdk";
 
 const googleClientId =
   "991569752579-gig8rttagf7jk8ihfdgst1p53kme3ah9.apps.googleusercontent.com";
 
 const App = () => {
   const isDarkMode = useColorScheme() === "dark";
+  const google = new GoogleOAuth();
+  const [, setCookie] = useCookies();
+
+  function setItem(name: string, value: string): void {
+    if (Platform.OS !== "web") return;
+    setCookie(name, value, {
+      expires: google.getExpiryDate(),
+    });
+  }
+
+  function handleLogin(response: any): void {
+    const google = new GoogleOAuth();
+
+    axios
+      .post(`${google.getBaseURL()}/auth/convert-token`, {
+        token: response.accessToken,
+        backend: "google-oauth2",
+        grant_type: "convert_token",
+        client_id: google.getClientID(),
+        client_secret: google.getClientSecret(),
+      })
+      .then((res) => {
+        const { access_token, refresh_token } = res.data;
+        console.log({ access_token, refresh_token });
+        setItem("access_token", access_token);
+        setItem("refresh_token", refresh_token);
+      })
+      .catch((err) => {
+        console.log("Error Google login", err);
+      });
+  }
 
   return (
     <SafeAreaView>
@@ -15,17 +54,8 @@ const App = () => {
       <GoogleLogin
         clientId={googleClientId}
         buttonText="LOGIN WITH GOOGLE"
-        onSuccess={response => handleLogin(response)}
-        render={renderProps => (
-          <button
-            onClick={renderProps.onClick}
-            disabled={renderProps.disabled}
-            type="button"
-            className="login-with-google-btn">
-            Sign in with Google
-          </button>
-        )}
-        onFailure={err => console.log('Google Login failed', err)}
+        onSuccess={handleLogin}
+        onFailure={(err) => console.log("Google Login failed", err)}
         isSignedIn={true}
       />
     </SafeAreaView>
--- vetkoek/src/utils/sdk.ts
+++ vetkoek/src/utils/sdk.ts
@@ -1,8 +1,4 @@
-import { Platform } from "react-native";
-import { useCookies } from "react-cookie";
-import axios from "axios";
-
-class GoogleOAuth {
+export class GoogleOAuth {
   private drfClientId = process.env.REACT_APP_DRF_CLIENT_ID;
   private drfClientSecret = process.env.REACT_APP_DRF_CLIENT_SECRET;
   private baseURL = "http://localhost:8000";
@@ -24,36 +20,4 @@ class GoogleOAuth {
     expiryDate.setMonth(expiryDate.getMonth() - 3);
     return expiryDate;
   }
-
-  public setItem(name: string, value: string): void {
-    if (Platform.OS !== "web") return;
-
-    const [, setCookie] = useCookies([name]);
-
-    setCookie(name, value, {
-      expires: this.getExpiryDate(),
-    });
-  }
-}
-
-export function handleLogin(response: any): void {
-  const google = new GoogleOAuth();
-
-  axios
-    .post(`${google.getBaseURL()}/auth/convert-token`, {
-      token: response.accessToken,
-      backend: "google-oauth2",
-      grant_type: "convert_token",
-      client_id: google.getClientID(),
-      client_secret: google.getClientSecret(),
-    })
-    .then((res) => {
-      const { access_token, refresh_token } = res.data;
-      console.log({ access_token, refresh_token });
-      google.setItem("access_token", access_token);
-      google.setItem("refresh_token", refresh_token);
-    })
-    .catch((err) => {
-      console.log("Error Google login", err);
-    });
 }
