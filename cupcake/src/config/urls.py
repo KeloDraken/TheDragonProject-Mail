c73e846bb4511528e4e4cb8ba95108d25effcd0f
Joy Mama-Kgosiemang <jmama021@student.wethinkcode.co.za>
2022-08-10 11:27:09 +0200

    Remove Group model from admin site

--- cupcake/src/config/urls.py
+++ cupcake/src/config/urls.py
@@ -2,9 +2,15 @@
 Project URLs
 """
 from django.contrib import admin
+from django.contrib.auth.models import Group
+
 from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("auth/", include("drf_social_oauth2.urls", namespace="drf")),
 ]
+
+admin.site.site_header = "KeloDraken Mail Admin"
+admin.site.site_title = "KeloDraken Mail Admin"
+admin.site.unregister(Group)
