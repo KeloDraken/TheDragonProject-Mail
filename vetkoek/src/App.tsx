86a1f1b64ee69a4dea05785b0a83cb9a0dff21b9
Joy Mama-Kgosiemang <jmama021@student.wethinkcode.co.za>
2022-08-11 10:03:03 +0200

    Wrap App component with GoogleOAuthProvider

--- vetkoek/src/App.tsx
+++ vetkoek/src/App.tsx
@@ -1,7 +1,15 @@
+import { GoogleOAuthProvider } from "@react-oauth/google";
 import { LandingScreen } from "./pages";
 
+const googleClientId: string =
+  "991569752579-gig8rttagf7jk8ihfdgst1p53kme3ah9.apps.googleusercontent.com";
+
 function App(): JSX.Element {
-  return <LandingScreen />;
+  return (
+    <GoogleOAuthProvider clientId={googleClientId}>
+      <LandingScreen />
+    </GoogleOAuthProvider>
+  );
 }
 
 export default App;
