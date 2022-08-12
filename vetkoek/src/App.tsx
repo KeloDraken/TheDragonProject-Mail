e8108ecee23cc68c0170824392d228fbcd2fa34b
Joy Mama-Kgosiemang <jmama021@student.wethinkcode.co.za>
2022-08-12 20:21:04 +0200

    Remove google auth provider

--- vetkoek/src/App.tsx
+++ vetkoek/src/App.tsx
@@ -1,9 +1,5 @@
 import { NavigationContainer } from "@react-navigation/native";
-import { GoogleOAuthProvider } from "@react-oauth/google";
 import Router from "./router";
-
-const googleClientId: string =
-  "991569752579-gig8rttagf7jk8ihfdgst1p53kme3ah9.apps.googleusercontent.com";
 
 const linking = {
   prefixes: ["http://localhost:8080", "https://mail.kelodraken.com"],
@@ -12,9 +8,7 @@ const linking = {
 function App(): JSX.Element {
   return (
     <NavigationContainer linking={linking}>
-      <GoogleOAuthProvider clientId={googleClientId}>
-        <Router />
-      </GoogleOAuthProvider>
+      <Router />
     </NavigationContainer>
   );
 }
