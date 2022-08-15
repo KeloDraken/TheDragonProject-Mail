20a22380390db8ae1a887bcdb79d5d16b7fb6920
Joy Mama-Kgosiemang <jmama021@student.wethinkcode.co.za>
2022-08-15 12:07:26 +0200

    Customise django allauth login page

--- cupcake/src/apps/accounts/views.py
+++ cupcake/src/apps/accounts/views.py
@@ -1,3 +1,4 @@
+from tempfile import template
 import urllib.parse
 
 from allauth.socialaccount.providers.google.views import GoogleOAuth2Adapter
@@ -62,6 +63,7 @@ class GoogleLogin(SocialLoginView):
     adapter_class = GoogleOAuth2Adapter
     callback_url = "http://127.0.0.1:8000/auth/google/callback"
     client_class = OAuth2Client
+
 
     def get_serializer(self, *args, **kwargs):
         serializer_class = self.get_serializer_class()
