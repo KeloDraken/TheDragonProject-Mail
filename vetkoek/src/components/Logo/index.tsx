703ec215dbbc911d7cbeffdf2182993a68f5cde4
Joy Mama-Kgosiemang <jmama021@student.wethinkcode.co.za>
2022-08-20 17:25:28 +0200

    Finish pricing page

--- vetkoek/src/components/Logo/index.tsx
+++ vetkoek/src/components/Logo/index.tsx
@@ -1,12 +1,15 @@
 import { View } from "react-native";
 import { styles } from "./styles";
 import { Text } from "../../components";
+import { Link } from "react-router-dom";
 
 function _Logo(): JSX.Element {
   return (
     <View style={styles.container}>
-      <Text style={styles.company}>kelodraken</Text>
-      <Text style={styles.product}>Mail</Text>
+      <Link to="/">
+        <Text style={styles.company}>kelodraken</Text>
+        <Text style={styles.product}>Mail</Text>
+      </Link>
     </View>
   );
 }
